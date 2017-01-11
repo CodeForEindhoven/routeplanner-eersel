225b74b5b351bd29698fe2f83754a8c82f72f150
milovanderlinden <milo@dogodigi.net>
2017-01-11 15:32:27 +0100

    Implementing gulp to build

---
+++ src/js/main.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', function() {
+  document.getElementById('re_search').addEventListener('keydown', function(event) {
+    console.log(event.which);
+  });
+});
